bfd04ce9b5c1ccd1408c9da1d7724c885fefa166
ScolnicArtur <35298047+ScolnicArtur@users.noreply.github.com>
2020-05-24 20:43:11 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<?php
+
+$host = 'sql7.freemysqlhosting.net';
+$user = 'sql7343022';
+$pass = 'NZKVkwW18G';
+$db = 'sql7343022';
+
+$connection = mysqli_connect($host,$user,$pass,$db);
+
+$query = "select * from meniu_filtrare";
+$result = mysqli_query($connection, $query);
+while ($row = $result->fetch_assoc()){
+    echo $row['nume_filtru'] . ' ' .  $row['subcategorii'];
+}
+
+?>
